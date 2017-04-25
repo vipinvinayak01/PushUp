394e7721e51538154a654940ab9cea26ba30385c
vipin <vipinvinayak01@gmail.com>
2017-04-25 16:02:28 +0530

    variable define inside testFile class

---
+++ app/src/main/java/com/vipin/pushup/MainActivity.java
@@ -0,0 +1,20 @@
+package com.vipin.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    private void testData()
+    {
+        int i = 1+1;
+
+    }
+}
---
+++ app/src/main/java/com/vipin/pushup/TestAddFile.java
@@ -0,0 +1,12 @@
+package com.vipin.pushup;
+
+/**
+ * Created by vipin on 4/25/2017.
+ */
+
+public class TestAddFile {
+
+
+    private int a;
+
+}
